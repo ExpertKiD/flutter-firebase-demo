9bf9e11d9943df4d20dacdbef537775669044a10
Suman Adhikari <expertxkid@gmail.com>
2022-01-18 18:07:15 +0545

    feat: add provider scope for material app

--- lib/apps/app.dart
+++ lib/apps/app.dart
@@ -2,17 +2,20 @@ import 'package:firebasedemo/screens/home.dart';
 import 'package:firebasedemo/themes/dark.dart';
 import 'package:firebasedemo/themes/light.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FirebaseDemoApp extends StatelessWidget {
   const FirebaseDemoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.light,
-      home: const HomeScreen(),
+    return ProviderScope(
+      child: MaterialApp(
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.light,
+        home: const HomeScreen(),
+      ),
     );
   }
 }
